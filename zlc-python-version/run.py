45f8edb1dbb07a8e41d4a74331f0545164f575c6
Nikolia558 <38259407+Nikolai558@users.noreply.github.com>
2022-01-23 17:12:38 -0700

    Fixed Window Layouts
    When the layouts are not in Column Elements, the visible property has no effect.
    Therefore, I Made each "screen" its own "Column"

--- zlc-python-version/run.py
+++ zlc-python-version/run.py
@@ -7,7 +7,11 @@ WINDOW_SIZE = (800, 500)
 
 # Create the layout for the window
 # Layouts must be ordered from most bottom to top otherwise window spacing issues happen.
-LAYOUT = [layouts.main_frame, layouts.msg_frame]
+LAYOUT = [[
+    sg.Column([layouts.progress_frame], key='-PRG-FRAME-', expand_x=True, expand_y=True, visible=False),
+    sg.Column([layouts.main_frame], key='-MAIN-FRAME-', expand_x=True, expand_y=True, visible=False),
+    sg.Column([layouts.msg_frame], key='-MSG-FRAME-', expand_x=True, expand_y=True, visible=True)
+]]
 
 
 class AssistantWindow:
@@ -28,6 +32,14 @@ class AssistantWindow:
             if event == '-MSG-CONTINUE-':
                 self.window['-MSG-FRAME-'].update(visible=False)
                 self.window['-MAIN-FRAME-'].update(visible=True)
+
+            if event == '-PRG-CONTINUE-':
+                self.window['-PRG-FRAME-'].update(visible=False)
+                self.window['-MAIN-FRAME-'].update(visible=True)
+
+            if event == 'A)':
+                self.window['-MAIN-FRAME-'].update(visible=False)
+                self.window['-PRG-FRAME-'].update(visible=True)
 
         self.window.close()
 
