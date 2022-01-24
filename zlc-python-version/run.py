4ec8899778f9e477693e98dff5337257c7f3703e
Nikolia558 <38259407+Nikolai558@users.noreply.github.com>
2022-01-23 17:26:51 -0700

    Added button functionality
    Also Added TODO's

--- zlc-python-version/run.py
+++ zlc-python-version/run.py
@@ -2,7 +2,6 @@ import PySimpleGUI as sg
 import gui_layouts as layouts
 
 # Static Window Variables.
-# TODO - Window_Size is not used right now.
 WINDOW_SIZE = (800, 500)
 
 # Create the layout for the window
@@ -17,6 +16,10 @@ LAYOUT = [[
 class AssistantWindow:
     def __init__(self):
         self.window = sg.Window('ZLC FE Assistant', LAYOUT, size=WINDOW_SIZE)
+
+    def update_check(self):
+        # TODO - Create and Implement Update_Check
+        pass
 
     def event_loop(self):
         """In charge of keeping GUI open and checking user actions."""
@@ -33,13 +36,52 @@ class AssistantWindow:
                 self.window['-MSG-FRAME-'].update(visible=False)
                 self.window['-MAIN-FRAME-'].update(visible=True)
 
+            # TEMP - Button to exit out of the Progress Screen
             if event == '-PRG-CONTINUE-':
                 self.window['-PRG-FRAME-'].update(visible=False)
                 self.window['-MAIN-FRAME-'].update(visible=True)
 
+            # User has chosen option 'A'
             if event == 'A)':
                 self.window['-MAIN-FRAME-'].update(visible=False)
                 self.window['-PRG-FRAME-'].update(visible=True)
+                # TODO - Create and Implement Split GeoMaps or VideoMaps Function
+
+            # User has chosen option 'B'
+            if event == 'B)':
+                self.window['-MAIN-FRAME-'].update(visible=False)
+                self.window['-PRG-FRAME-'].update(visible=True)
+                # TODO - Create and Implement FE Buddy Export and Transfer Function
+
+            # User has chosen option 'C'
+            if event == 'C)':
+                self.window['-MAIN-FRAME-'].update(visible=False)
+                self.window['-PRG-FRAME-'].update(visible=True)
+                # TODO - Create and Implement Change Data Release Date in Alias and vERAM Function
+
+            # User has chosen option 'D'
+            if event == 'D)':
+                self.window['-MAIN-FRAME-'].update(visible=False)
+                self.window['-PRG-FRAME-'].update(visible=True)
+                # TODO - Create and Implement Combine GeoMaps or VideoMaps into single xml Function.
+
+            # User has chosen option 'E'
+            if event == 'E)':
+                self.window['-MAIN-FRAME-'].update(visible=False)
+                self.window['-PRG-FRAME-'].update(visible=True)
+                # TODO - Create and Implement Combine and Transfer SCT Files Function
+
+            # User has chosen option 'F'
+            if event == 'F)':
+                self.window['-MAIN-FRAME-'].update(visible=False)
+                self.window['-PRG-FRAME-'].update(visible=True)
+                # TODO - Create and Implement Combine and Transfer Alias files Function
+
+            # User has chosen option 'G'
+            if event == 'G)':
+                self.window['-MAIN-FRAME-'].update(visible=False)
+                self.window['-PRG-FRAME-'].update(visible=True)
+                # TODO - Create and Implement Transfer to Public Function
 
         self.window.close()
 
